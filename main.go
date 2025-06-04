0718cadb5dfa3768409a86439a2fcf06a7c0169b
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-06-05 00:39:10 +0700

    test: open server

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"net/http"
+	"log"
+	"os"
 )
 
 func main() {
-	http.ListenAndServe(":5000", nil);
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "5000"
+	}
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	});
+
+	log.Println("Server starting on port " + port)
+
+	err := http.ListenAndServe(":" + port, nil);
+	if err != nil {
+		log.Fatal(err)
+	}
 }
